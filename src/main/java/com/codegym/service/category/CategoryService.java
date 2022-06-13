ba60f38d84f9a1f166faa05b7d25611e78ea530a
Loc <daoloc93@gmail.com>
2022-06-13 09:28:36 +0700

    first commit

---
+++ src/main/java/com/codegym/service/category/CategoryService.java
@@ -0,0 +1,8 @@
+package com.codegym.service.category;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+}
---
+++ src/main/java/com/codegym/service/product/ProductService.java
@@ -0,0 +1,42 @@
+package com.codegym.service.product;
+
+import com.codegym.model.Product;
+import com.codegym.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService implements IProductService{
+
+    @Autowired
+    IProductRepository productRepository;
+
+    @Override
+    public Iterable<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void save(Product product) {
+
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+
+    @Override
+    public Iterable<Product> findAllByNameContaining(String name) {
+        return productRepository.findAllByName;
+    }
+}
